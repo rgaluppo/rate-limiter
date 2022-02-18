9d8ed51b491fc771d6fea7360d4e1420048626c5
rgaluppo <raul.i.galuppo@gmail.com>
2022-02-18 09:29:35 -0300

    auth middle

---
+++ src/middlewares/auth.spec.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express';
+import { authMiddleware } from './auth';
+
+describe('Auth middle', () => {
+    const mockResponse: Partial<Response> = { json: jest.fn() };
+    const nextFunction: NextFunction = jest.fn();
+    it('continue the request', () => {
+        const mockRequest = {
+            get: jest.fn(name => {
+                if(name === 'user-id') {return  '1111'; }
+                return;
+            }),
+        };
+        authMiddleware(mockRequest as unknown as Request, mockResponse as Response, nextFunction);
+        expect(nextFunction).toHaveBeenCalledWith();
+    });
+    it('stop the request and go to the error middleware', () => {
+        const mockRequest: Partial<Request> = { get: jest.fn() };
+        authMiddleware(mockRequest as unknown as Request, mockResponse as Response, nextFunction);
+        expect(nextFunction).toHaveBeenCalledWith(new Error('User not logged!'))
+    });
+})
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import { errorMiddleware, notFoundMiddlware } from '../middlewares/error';
+import { authMiddleware } from "../middlewares/auth";
 
 export default function (app:any) {
     app.use(helmet());
     app.use(cors());
     app.use(express.json());
+    app.use(authMiddleware)
     app.use('*', notFoundMiddlware);
     app.use(errorMiddleware);
 };
